ddae8a79a2aa2af412d722a95b606344cad25ad7
lixinyang123 <lixinyangemil@outlook.com>
2024-11-07 19:45:18 +0800

    add emails api

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -4,40 +4,74 @@
  * For more information, see https://remix.run/file-conventions/entry.server
  */
 
-import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare";
-import { RemixServer } from "@remix-run/react";
-import { isbot } from "isbot";
-import { renderToReadableStream } from "react-dom/server";
+import type { AppLoadContext, EntryContext } from "@remix-run/cloudflare"
+import { RemixServer } from "@remix-run/react"
+import { isbot } from "isbot"
+import { renderToReadableStream } from "react-dom/server"
+import { d1Wrapper } from "~/.server/db"
+import { sessionWrapper } from "~/.server/session"
 
 export default async function handleRequest(
 	request: Request,
 	responseStatusCode: number,
 	responseHeaders: Headers,
 	remixContext: EntryContext,
-	// This is ignored so we can keep it in the template for visibility.  Feel
-	// free to delete this parameter in your app if you're not using it!
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	loadContext: AppLoadContext,
+	loadContext: AppLoadContext
 ) {
+	let url = new URL(request.url).pathname
+
+	switch (true) {
+		case url.startsWith('/api'):
+			return handleApi(request, loadContext)
+	
+		default:
+			return handlePage(request, responseStatusCode, responseHeaders, remixContext)
+	}
+}
+
+async function handlePage(request: Request, responseStatusCode: number,
+	responseHeaders: Headers, remixContext: EntryContext) {
 	const body = await renderToReadableStream(
 		<RemixServer context={remixContext} url={request.url} />,
 		{
 			signal: request.signal,
 			onError(error: unknown) {
 				// Log streaming rendering errors from inside the shell
-				console.error(error);
-				responseStatusCode = 500;
-			},
-		},
-	);
+				console.error(error)
+				responseStatusCode = 500
+			}
+		}
+	)
 
 	if (isbot(request.headers.get("user-agent") || "")) {
-		await body.allReady;
+		await body.allReady
 	}
 
-	responseHeaders.set("Content-Type", "text/html");
+	responseHeaders.set("Content-Type", "text/html")
+
 	return new Response(body, {
 		headers: responseHeaders,
-		status: responseStatusCode,
-	});
+		status: responseStatusCode
+	})
+}
+
+async function handleApi(request: Request, context: AppLoadContext) {
+	const db = d1Wrapper(context.cloudflare.env.DB)
+	const { getSession } = sessionWrapper(context.cloudflare.env)
+
+	const session = await getSession(request.headers.get("Cookie"))
+
+	let emails = await db.query.emails.findMany({
+		columns: {
+			id: true,
+			subject: true,
+			createdAt: true
+		},
+		where: (emails, { eq }) => eq(emails.messageTo, session.data.email),
+		orderBy(fields, operators) {
+			return [operators.desc(fields.createdAt)]
+		}
+	})
+
+	return new Response(JSON.stringify(emails))
 }
